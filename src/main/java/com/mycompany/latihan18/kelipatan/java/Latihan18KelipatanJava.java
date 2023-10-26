883b47879f9b5bc9827118c75f25870de0c86e0b
Zalfarsyh9 <145966652+Zalfarsyh9@users.noreply.github.com>
2023-10-26 11:51:52 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan18/kelipatan/java/Latihan18KelipatanJava.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan18.kelipatan.java;
+
+/**
+ *
+ * @author User
+ */
+public class Latihan18KelipatanJava {
+
+    public static void main(String[] args) {
+        
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println(i);
+    }
+    }
+    
+}
